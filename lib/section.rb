2c8cdc8afb0b066c26d3a0986c2c9f03ca042681
PedroSena <sena.pedro@gmail.com>
2013-04-13 19:29:46 -0300

    Listing files accordingly but more tests are required

--- lib/section.rb
+++ lib/section.rb
@@ -1,7 +1,29 @@
 class Section
-  attr_accessor :valid_files
+  attr_accessor :name
 
-  def initialize(name,base_dir,files_matching,ignore_files_matching = [])
-
+  def initialize(name,base_dir,files_matching,ignore_files_matching = nil)
+    @name = name
+    @base_dir = base_dir
+    @files_matching = files_matching
+    @ignore_files_matching = ignore_files_matching || []
   end
+
+  def valid_files
+    all_files.find_all{ |file| is_valid?(file) }.find_all{ |file| ! ignore?(file) }
+  end
+
+  private
+  def all_files
+    Dir.glob(@base_dir + '/**/*')
+  end
+
+  def ignore?(relative_path)
+    return false if @ignore_files_matching.length < 1
+    @ignore_files_matching.find_all{|regex| relative_path.match regex }.length > 0
+  end
+
+  def is_valid?(relative_path)
+    @files_matching.find_all{|regex| relative_path.match regex }.length > 0
+  end
+
 end
