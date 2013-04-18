51f7c28826f93553330a301a5620961cb18d89ab
PedroSena <sena.pedro@gmail.com>
2013-04-18 11:35:17 -0300

    Fixed issues related to daemon call from outside its dir

--- config_history.rb
+++ config_history.rb
@@ -2,9 +2,9 @@ require 'rubygems'
 require 'bundler/setup'
 require 'daemons'
 
-require './lib/configuration_reader'
-require './lib/git_gateway'
-require './lib/section'
+require_relative './lib/configuration_reader'
+require_relative './lib/git_gateway'
+require_relative './lib/section'
 
 INSTALL_DIR = File.expand_path(File.dirname(__FILE__))
-Daemons.run('lib/runner.rb')
+Daemons.run( INSTALL_DIR + '/lib/runner.rb', dir: '/tmp', dir_mode: :normal, ontop: false, log_output: true)
